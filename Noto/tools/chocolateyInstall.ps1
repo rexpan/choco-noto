6311a6f9afe150027b4549cb662175854a542880
Rex Pan <napxer@gmail.com>
2021-08-31 11:25:32 +0700

    Upgrade to use chocolatey-font-helpers.extension

--- Noto/tools/chocolateyInstall.ps1
+++ Noto/tools/chocolateyInstall.ps1
@@ -1,12 +1,3 @@
-function Get-CurrentDirectory
-{
-  $thisName = $MyInvocation.MyCommand.Name
-  [IO.Path]::GetDirectoryName((Get-Content function:$thisName).File)
-}
-
-$fontHelpersPath = (Join-Path (Get-CurrentDirectory) 'FontHelpers.ps1')
-. $fontHelpersPath
-
 $fontUrl = 'https://noto-website-2.storage.googleapis.com/pkgs/Noto-hinted.zip'
 $checksumType = 'sha256';
 $checksum = '837B4A9352FCE32AD7F298FBF155AF1DA5B6F3F8DBD995EB63FDD8E82117E4AE';
@@ -15,35 +6,20 @@ $destination = Join-Path $Env:Temp 'NotoFonts'
 
 Install-ChocolateyZipPackage -PackageName 'NotoFonts' -url $fontUrl -unzipLocation $destination -ChecksumType "$checksumType" -Checksum "$checksum"
 
-$shell = New-Object -ComObject Shell.Application
-$fontsFolder = $shell.Namespace(0x14)
+$FontFiles = (
+  (Get-ChildItem $destination -Include '*.otf' -Recurse) +
+  (Get-ChildItem $destination -Include '*.ttf' -Recurse)
+  ) | Select-Object -ExpandProperty FullName
 
-$fontFiles = (Get-ChildItem $destination -Recurse -Filter *.otf) +
-             (Get-ChildItem $destination -Recurse -Filter *.ttf)
+$Installed = Add-Font $FontFiles -Multiple
 
-$fss = for($i=0; $i -lt $fontFiles.length; $i+=10){
-    ,($fontFiles[$i .. ($i+9)])
-}
-
-for($i=0; $i -lt $fss.length; $i+=1){
-    $fs = $fss[$i];
-
-    # unfortunately the font install process totally ignores shell flags :(
-    # http://social.technet.microsoft.com/Forums/en-IE/winserverpowershell/thread/fcc98ba5-6ce4-466b-a927-bb2cc3851b59
-    # so resort to a nasty hack of compiling some C#, and running as admin instead of just using CopyHere(file, options)
-    $commands = $fs |
-    % { Join-Path $fontsFolder.Self.Path $_.Name } |
-    ? { Test-Path $_ } |
-    % { "Remove-SingleFont '$_' -Force;" }
-
-    # http://blogs.technet.com/b/deploymentguys/archive/2010/12/04/adding-and-removing-fonts-with-windows-powershell.aspx
-    $fs |
-    % { $commands += "Add-SingleFont '$($_.FullName)';" }
-
-    $toExecute = ". $fontHelpersPath;" + ($commands -join ';')
-    Start-ChocolateyProcessAsAdmin $toExecute
+If ($Installed -eq 0) {
+   Throw 'All font installation attempts failed!'
+} elseif ($Installed -lt $FontFiles.count) {
+   Write-Host "$Installed fonts were installed." -ForegroundColor Cyan
+   Write-Warning "$($FontFiles.count - $Installed) submitted font paths failed to install."
+} else {
+   Write-Host "$Installed fonts were installed."
 }
 
 Remove-Item $destination -Recurse
-
-
